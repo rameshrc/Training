8a8a9b707983eb311e06e4b48cae91363502215e
Ramesh Ramachandran <10135431@bd.com>
2017-12-26 21:37:59 -0800

    jsdemo WIP

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./index.css";
+import numeral from 'numeral';
+const courseValue= numeral(1000).format('$0,0.00');
+//debugger;
+console.log(`I would pay ${courseValue} for this course`);//eslint-disable-line no-console
